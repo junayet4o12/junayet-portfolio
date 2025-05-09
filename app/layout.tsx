dad4983bc34749ef02b806ccb9620311d603a0ec
Junayet Alam <muhammadjunayetmaruf@gmail.com>
2025-05-09 10:50:07 +0600

    meta data has added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,71 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: `Junayet's Portfolio`,
+  title: "Junayet Alam | Full-Stack Web Developer",
+  description: "Explore Junayet Alam's portfolio showcasing expertise in React, Node.js, and full-stack web development.",
+  keywords: [
+    "Junayet Alam",
+    "Junayet",
+    "Junaid",
+    "Junaid Alam",
+    "Junayed Alam",
+    "Full-Stack Developer",
+    "React Developer",
+    "Node.js Developer",
+    "MERN Stack",
+    "MERN Developer",
+    "Full-Stack Engineer",
+    "Backend Developer",
+    "Web Developer",
+    "Software Engineer",
+    "Next.js Developer",
+    "JavaScript Developer",
+    "Express.js",
+    "MongoDB",
+    "REST API",
+    "Portfolio",
+    "Web Development",
+    "Programming",
+    "Developer Portfolio"
+  ],
+  authors: [{ name: "Junayet Alam", url: "https://junayet-alam-portfolio.vercel.app/" }],
+  creator: "Junayet Alam",
+  publisher: "Junayet Alam",
+  robots: "index, follow",
   icons: {
-    icon: '/favicon.svg',
+    icon: "/favicon.svg",
   },
-}
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://junayet-alam-portfolio.vercel.app/",
+    siteName: "Junayet Alam Portfolio",
+    title: "Junayet Alam | Full-Stack Web Developer",
+    description: "Explore Junayet Alam's portfolio showcasing expertise in React, Node.js, and full-stack web development.",
+    images: [
+      {
+        url: "/profile.png",
+        width: 800,
+        height: 600,
+        alt: "Junayet Alam"
+      }
+    ],
+    emails: ["muhammadjunayetmaruf@gmail.com"],
+    phoneNumbers: ["+8801632884012"],
+    countryName: "Bangladesh",
+    alternateLocale: [
+      "https://www.linkedin.com/in/junayet-alam/",
+       "https://github.com/junayet4o12",
+      "https://www.facebook.com/junayet4012",
+      "https://wa.me/+8801632884012"
+    ]
+  },
+  alternates: {
+    canonical: "https://junayet-alam-portfolio.vercel.app/"
+  },
+  generator: "Next.js",
+};
+
 import { ThemeProvider } from "@/components/theme-provider"
 export default function RootLayout({
   children,
--- components/resume.tsx
+++ components/resume.tsx
@@ -1,50 +1,19 @@
-// import { Document, Page, pdfjs } from 'react-pdf';
-import { useState } from 'react';
-import { ArrowRight, X } from "lucide-react";
+'use client'
+import { ArrowRight } from "lucide-react";
 import { Button } from "./ui/button";
-import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogClose
-} from "./ui/dialog";
-import { ScrollArea } from "./ui/scroll-area";
 export default function Resume() {
-    const [open, setOpen] = useState(false);
+    const handleShowResume = () => {
+        window.open("/junayet-Resume.pdf", "_blank");
+    }
     return (
-        <>
-            <Button onClick={() => setOpen(true)} className="group md:h-12 md:text-base relative px-3 md:px-4">
-                Resume
-                <span className="bg-foreground size-6 md:size-8 flex justify-center items-center rounded-lg absolute right-3  md:right-4 group-hover:right-2 md:group-hover:right-3 transition-all duration-300">
-                    <ArrowRight className="" size={16} />
-                </span>
-                <span className=" size-6 md:size-8 flex justify-center items-center rounded-lg bg-transparent text-transparent">
-                    <ArrowRight className="" size={16} />
-                </span>
-            </Button>
-
-            <Dialog open={open} onOpenChange={setOpen}>
-                <DialogContent className="!max-w-7xl w-[95vw] max-h-[90vh] p-0">
-                    <DialogHeader className="p-4 flex flex-row items-center justify-between border-b sticky top-0 bg-background z-10">
-                        <DialogTitle className="text-xl font-semibold">Resume</DialogTitle>
-                        <DialogClose asChild>
-                            <Button variant="ghost" size="icon" className="rounded-full">
-                                <X size={18} />
-                            </Button>
-                        </DialogClose>
-                    </DialogHeader>
-                    <ScrollArea className="flex-1 w-full h-[80vh]">
-                        <div className="flex-1 w-full h-full overflow-hidden">
-                            <iframe
-                                src={`/junayet-resume.pdf`}
-                                className="w-full h-[75vh]"
-                                title="Resume"
-                            />
-                        </div>
-                    </ScrollArea>
-                </DialogContent>
-            </Dialog>
-        </>
+        <Button onClick={handleShowResume} className="group lg:h-12 lg:text-base relative px-3 lg:px-4">
+            Resume
+            <span className="bg-foreground size-6 lg:size-8 flex justify-center items-center rounded-lg absolute right-3  lg:right-4 group-hover:right-2 lg:group-hover:right-3 transition-all duration-300">
+                <ArrowRight className="" size={16} />
+            </span>
+            <span className=" size-6 lg:size-8 flex justify-center items-center rounded-lg bg-transparent text-transparent">
+                <ArrowRight className="" size={16} />
+            </span>
+        </Button>
     );
 }
