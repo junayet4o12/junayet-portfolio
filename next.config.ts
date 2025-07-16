48be16b2cbabec8b488e43e270e7eb4612c3da0a
Junayet Alam <muhammadjunayetmaruf@gmail.com>
2025-07-17 00:31:24 +0600

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
-  },
+ 
 };
 
 export default nextConfig;
